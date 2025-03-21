4f4c7b1d676280b067aaa5b39709124e0a25825c
saninkaz <guardbro85@gmail.com>
2025-03-21 19:10:58 +0530

    feat:added:added-data-storage

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig } from 'drizzle-kit';
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") }); // Go one level up
+
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     dialect: "postgresql",
@@ -7,9 +12,9 @@ export default defineConfig({
     dbCredentials: {
         host: "localhost",
         port: 5432,
-        database: "pediaversedb",
-        user: "sanin",
-        password: "sanin",
+        database: process.env.POSTGRES_DB || "pediaversedb",
+        user: process.env.POSTGRES_USER || "sanin",
+        password: process.env.POSTGRES_PASSWORD || "sanin",
         ssl: false,
     },
 });
